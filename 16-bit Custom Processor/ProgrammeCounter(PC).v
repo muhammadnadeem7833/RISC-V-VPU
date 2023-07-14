a3aee91782453284c6d181a1835eb4fec31477ff
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-14 17:20:03 +0500

    Create ProgrammeCounter(PC).v
    Program Counter Module

---
+++ 16-bit Custom Processor/ProgrammeCounter(PC).v
@@ -0,0 +1,14 @@
+module PC(out,clk,rstn);
+  output [3:0] out;//By-default wire
+  reg [7:0] out;
+  input rstn, clk;
+
+  always@(posedge clk or negedge rstn)//rstn is used here as active low
+  begin
+    if (!rstn)//rstn == 0 reset the circuit 
+      out <= 0;
+    else      //rstn == 1 normal working
+      out <= out + 1;    
+  end
+
+endmodule
