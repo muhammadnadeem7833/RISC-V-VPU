24f3f899b67dfb2c9723af11c943e4fd1a64e6a9
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-15 02:32:58 +0500

    Add files via upload

---
+++ 16-bit Custom Processor/Data_Memory.v
@@ -0,0 +1,17 @@
+module DM(wflag,ALU_res,datain,dataout);
+  input wflag; //flag to read or write
+  input [15:0] ALU_res; //final address after adition
+  input [15:0] datain; //data to be write on address
+  output reg [15:0] dataout; //data to be read from address and sent out
+  reg [15:0] Data_Mem[0:65535]; //Data memory array
+ 
+  //to read/write data
+  always @(*)
+  begin
+    if(wflag)
+      Data_Mem[ALU_res]=datain; //write data
+    else
+      dataout=Data_Mem[ALU_res]; //raed data
+  end
+
+endmodule
