4ea04cfdfb26631bebca11282a40c1ed75cf0d00
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-15 01:48:27 +0500

    Update Instruction_Register.v

--- 16-bit Custom Processor/Instruction_Register.v
+++ 16-bit Custom Processor/Instruction_Register.v
@@ -1,10 +1,10 @@
 module IR(in_inst,out_inst,clk);
-  input [15:0] in_inst;
+  input [15:0] in_inst; //instruction from ROM
   input clk;
-  output reg [15:0] out_inst;
+  output reg [15:0] out_inst; //output register
 
-  always @(posedge clk)
+  always @(posedge clk) //run on positive edge only
   begin
-    out_inst <= in_inst;
+    out_inst <= in_inst; //value assigning(non-blocking statement)
   end
 endmodule
