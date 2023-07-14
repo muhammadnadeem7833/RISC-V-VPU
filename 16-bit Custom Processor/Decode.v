3fc2698657facfa151eae86a05d6d7000b82a5db
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-15 02:05:21 +0500

    Update Decode.v

--- 16-bit Custom Processor/Decode.v
+++ 16-bit Custom Processor/Decode.v
@@ -1,7 +1,6 @@
-module Decode(instr,opcode,mem_sig,reg_sig1,reg_sig2,reg_sig3,pc_sig);
+module Decode(instr,opcode,reg_sig1,reg_sig2,reg_sig3,pc_sig);
   input [15:0] instr; //from instruction register
   output [3:0] opcode; //opcode to CU
-  output [5:0] mem_sig; //for immediate value or memory to read
   output [2:0] reg_sig1; //when only 1 source register is used
   output [5:0] reg_sig2; //when only 2 source registers are used
   output [2:0] reg_sig3; //for destination register
@@ -9,7 +8,6 @@ module Decode(instr,opcode,mem_sig,reg_sig1,reg_sig2,reg_sig3,pc_sig);
   
   //assigning to all appriopriate bits
   assign opcode=instr[3:0];
-  assign mem_sig=instr[15:10];
   assign reg_sig1=instr[9:7];
   assign reg_sig2=instr[12:7];
   assign reg_sig3=instr[6:4];
