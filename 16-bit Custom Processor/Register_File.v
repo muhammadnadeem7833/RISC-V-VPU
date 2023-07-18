649e510cbf72d67dab1b18d4105e2aa80d0cf436
SajidGulKhawaja <70131691+SajidGulKhawaja@users.noreply.github.com>
2023-07-18 16:29:15 +0500

    Register_File.v

--- 16-bit Custom Processor/Register_File.v
+++ 16-bit Custom Processor/Register_File.v
@@ -1,20 +1,25 @@
-module RF(,RegWrite_Flag,Rreg1,Rreg2,Wreg,Datain,Dout1,Dout2);
+module RF(RegWrite_Flag,Rreg1,Rreg2,Wreg,Datain,Dout1,Dout2);
   input RegWrite_Flag;
   input [2:0] Rreg1;
   input [2:0] Rreg2;
   input [2:0] Wreg; 
   input [15:0] Datain;
+  input clk, rstn;
   output [15:0] Dout1;
   output [15:0] Dout2;
   reg [15:0] RegMem[0:7];
-  
+  integer i = 0;
   assign Dout1 = RegMem[Rreg1];
   assign Dout2 = RegMem[Rreg2];
   
-  always @(*)
+  always @(posedge clk or negedge rstn)
   begin
-    if(RegWrite_Flag)
-      RegMem[Wreg] = Datain;
+    if (!rstn)
+      for(i = 0; i < 8; i = i + 1)
+        RegMem[i] <= 0;
+    else
+      if(RegWrite_Flag)
+        RegMem[Wreg] = Datain;
   end
 
 endmodule
