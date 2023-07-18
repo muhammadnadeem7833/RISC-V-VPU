98e89089d2ac0325ca30280f0ff2daac2faaffe9
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-18 15:47:42 +0500

    Update ProgrammeCounter(PC).v

--- 16-bit Custom Processor/ProgrammeCounter(PC).v
+++ 16-bit Custom Processor/ProgrammeCounter(PC).v
@@ -1,6 +1,6 @@
-module PC(out,clk,rstn);
-  output [7:0] out;//By-default wire
-  reg [7:0] out;
+module PC(in,out,clk,rstn);
+  input [7:0] in;
+  output reg [7:0] out;
   input rstn, clk;
 
   always@(posedge clk or negedge rstn)//rstn is used here as active low
@@ -8,7 +8,7 @@ module PC(out,clk,rstn);
     if (!rstn)//rstn == 0 reset the circuit 
       out <= 0;
     else      //rstn == 1 normal working
-      out <= out + 1;    
+      out <= in;    
   end
 
 endmodule
