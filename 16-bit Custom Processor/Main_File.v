80d7b9c523441af9f9ff8dd3a36c4c0248f02123
SajidGulKhawaja <70131691+SajidGulKhawaja@users.noreply.github.com>
2023-07-18 16:38:32 +0500

    Main_File.v

--- 16-bit Custom Processor/Main_File.v
+++ 16-bit Custom Processor/Main_File.v
@@ -1,7 +1,7 @@
 module main(clk, rstn);
   input clk, rstn;
-  input [7:0] pc_out;
-  output [7:0] pc_in;
+  wire [7:0] pc_out;  //Output of PC
+  wire [7:0] pc_in;   //Generated in this module
 
   assign pc_in=pc_out+1;
 
