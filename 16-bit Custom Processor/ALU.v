10509cab42cd0520e397a57d2d1eea37be3c3e43
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-19 18:31:16 +0500

    Update ALU.v

--- 16-bit Custom Processor/ALU.v
+++ 16-bit Custom Processor/ALU.v
@@ -1,20 +1,21 @@
-module ALU(in1,in2,out,con_sig,hi,lo);
+module ALU(in1, in2, con_sig, out);
+//2 operands of 16-bit
   input [15:0] in1;
   input [15:0] in2;
-  input [2:0] con_sig;
-  output reg [15:0] out;
-  output reg [15:0] hi;
-  output reg [15:0] lo;
+
+  input [1:0] con_sig; //operation selection bits
+  output [15:0] out; //result
+
+  reg [15:0] out;
+  
 
   always @(*)
   begin
     case(con_sig)
-    3'b000: out = in1 + in2;
-    3'b001: out = in1 - in2;
-    3'b010: {hi,lo} = in1 * in2;
-    3'b011: out = in1 / in2;
-    3'b100: out = in1 & in2;
-    3'b101: out = in1 | in2; 
+    2'b00: out = in1 + in2; //addition
+    //2'b01: out = in1 * in2; //subtraction
+    2'b10: out = in1 & in2; //and
+    2'b11: out = in1 | in2; //or
     endcase
   end
 
