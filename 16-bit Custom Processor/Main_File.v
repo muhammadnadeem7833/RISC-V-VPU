17bfb66e25f7e0aac53b5392a6a3276cf5a67b37
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-19 18:33:14 +0500

    Main_File.v

--- 16-bit Custom Processor/Main_File.v
+++ 16-bit Custom Processor/Main_File.v
@@ -1,10 +1,1 @@
-module main(clk, rstn);
-  input clk, rstn;
-  wire [7:0] pc_out;  //Output of PC
-  wire [7:0] pc_in;   //Generated in this module
 
-  assign pc_in=pc_out+1;
-
-  PC obj_pc (pc_in, pc_out, clk, rstn);
-  
-endmodule
