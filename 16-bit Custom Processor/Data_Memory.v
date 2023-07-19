da4c0feb11b06677c979d1f23dbcbab2c9d73bf1
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-19 18:31:35 +0500

    Update Data_Memory.v

--- 16-bit Custom Processor/Data_Memory.v
+++ 16-bit Custom Processor/Data_Memory.v
@@ -1,17 +1,31 @@
-module DM(wflag,ALU_res,datain,dataout);
+module DM(wflag, ALU_res, datain, clk, rstn, dataout);
+
   input wflag; //write flag
-  input [15:0] ALU_res; //final address after adition
-  input [15:0] datain; //data to be write on address
-  output reg [15:0] dataout; //data to be read from address and sent out
+  input [15:0] ALU_res; //final address after adition from ALU
+  input [15:0] datain; //data to be write on final address
+  input clk, rstn;
+
+  output [15:0] dataout; //data to be read from address and send
+
   reg [15:0] Data_Mem[0:65535]; //Data memory array
+  reg [15:0] dataout;
+  integer i;
  
-  //to read/write data
-  always @(*)
+ 
+  always @(posedge clk or negedge rstn)
   begin
-    if(wflag)
-      Data_Mem[ALU_res]=datain; //write data
+    if(!rstn)
+//initialize all memory locations with 0 value
+      for(i = 0; i < 65536; i = i + 1)
+        Data_Mem[i] <= 0;
     else
-      dataout=Data_Mem[ALU_res]; //raed data
+//to read/write data from/to memory
+      begin
+        if(wflag)
+          Data_Mem[ALU_res] <= datain; //write data
+        else
+          dataout <= Data_Mem[ALU_res]; //raed data
+      end
   end
 
 endmodule
