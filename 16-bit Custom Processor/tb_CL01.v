7d653fa1d5f3ace470210a3198a11caa91bbe51b
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-19 18:34:11 +0500

    Update tb_CL01.v

--- 16-bit Custom Processor/tb_CL01.v
+++ 16-bit Custom Processor/tb_CL01.v
@@ -5,18 +5,26 @@ module tb_CL01();
   wire [7:0] instr_addr;
   wire  PC_select;
   wire [7:0] Jump_addr;
-  wire Source2_select, ALU_out_Select;
+  wire Source2_select, ALU_out_Select, RegWrite_Flag;
   wire [5:0] Immediate_Addr;
   wire [2:0] Rreg_Sig1, Rreg_Sig2, Wreg_Sig;
   wire [3:0] OPCODE;
 
-  Control_logic_01 obj_CL01(clk, rstn, instr_addr, PC_select, Source2_select, ALU_out_Select, Jump_addr, Immediate_Addr, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
+  Control_logic_01 obj_CL01(clk, rstn, instr_addr, PC_select, Source2_select, ALU_out_Select, RegWrite_Flag, Jump_addr, Immediate_Addr, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
 
   initial
   begin
     clk = 0; rstn = 0;
     #5
     clk = 1; rstn = 0;
+    #5
+    clk = 0; rstn = 1;
+    #5
+    clk = 1; rstn = 1;
+    #5
+    clk = 0; rstn = 1;
+    #5
+    clk = 1; rstn = 1;
     #5
     clk = 0; rstn = 1;
     #5
