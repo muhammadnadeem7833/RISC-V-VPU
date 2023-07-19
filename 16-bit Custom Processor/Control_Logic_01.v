19d37c4d44e05f8ed85db25d8a457198c1c6a3b3
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-19 18:31:56 +0500

    Update Control_Logic_01.v

--- 16-bit Custom Processor/Control_Logic_01.v
+++ 16-bit Custom Processor/Control_Logic_01.v
@@ -1,8 +1,8 @@
-module Control_logic_01(clk, rstn, instr_addr, PC_select, Source2_select, ALU_out_Select, Jump_addr, Immediate_Addr, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
+module Control_logic_01(clk, rstn, instr_addr, PC_select, Source2_select, ALU_out_Select, RegWrite_Flag, Jump_addr, Immediate_Addr, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
   input clk, rstn; //inputs
  //outputs of module
   output [7:0] instr_addr;
-  output PC_select, Source2_select, ALU_out_Select;
+  output PC_select, Source2_select, ALU_out_Select, RegWrite_Flag;
   output [7:0] Jump_addr;
   output [5:0] Immediate_Addr;
   output [2:0] Rreg_Sig1, Rreg_Sig2, Wreg_Sig;
@@ -21,16 +21,17 @@ module Control_logic_01(clk, rstn, instr_addr, PC_select, Source2_select, ALU_ou
   wire [15:0] instruction1;
   IR obj_IR(instruction, clk, rstn, instruction1);
 
-  wire source2_select, ALU_out_select;
+  wire source2_select, ALU_out_select,regwrite_flag;
   wire [5:0] immediate_addr;
   wire [2:0] Rreg_sig1, Rreg_sig2, Wreg_sig;
   wire [3:0] opcode;
-  Decode obj_Dec(instruction1, pc_select, source2_select, ALU_out_select, jump_addr, immediate_addr, Rreg_sig1, Rreg_sig2, Wreg_sig, opcode);
+  Decode obj_Dec(instruction1, pc_select, source2_select, ALU_out_select, regwrite_flag, jump_addr, immediate_addr, Rreg_sig1, Rreg_sig2, Wreg_sig, opcode);
 //assigning output 
   assign instr_addr = inst_address;
   assign PC_select = pc_select;
   assign Source2_select = source2_select;
   assign ALU_out_Select = ALU_out_select;
+  assign RegWrite_Flag = regwrite_flag;
   assign Jump_addr = jump_addr;
   assign Immediate_Addr = immediate_addr;
   assign Rreg_Sig1 = Rreg_sig1;
