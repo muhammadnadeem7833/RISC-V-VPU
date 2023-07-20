07377ac75db340f8fa54c65813c19a18b9a46bcc
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-20 13:00:01 +0500

    Update tb_CL01.v

--- 16-bit Custom Processor/TestBench/tb_CL01.v
+++ 16-bit Custom Processor/TestBench/tb_CL01.v
@@ -3,6 +3,7 @@ module tb_CL01();
   reg clk, rstn;
 //outputs
   wire [7:0] instr_addr;
+  wire [15:0] Instruction;
   wire  PC_select;
   wire [7:0] Jump_addr;
   wire Source2_select, ALU_out_Select, RegWrite_Flag;
@@ -10,25 +11,17 @@ module tb_CL01();
   wire [2:0] Rreg_Sig1, Rreg_Sig2, Wreg_Sig;
   wire [3:0] OPCODE;
 
-  Control_logic_01 obj_CL01(clk, rstn, instr_addr, PC_select, Source2_select, ALU_out_Select, RegWrite_Flag, Jump_addr, Immediate_Addr, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
+  Control_logic_01 obj_CL01(clk, rstn, instr_addr, Instruction, PC_select, Source2_select, ALU_out_Select, RegWrite_Flag, Jump_addr, Immediate_Addr, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
+
+  always
+  #5
+  clk = !clk;
 
   initial
   begin
     clk = 0; rstn = 0;
-    #5
-    clk = 1; rstn = 0;
-    #5
-    clk = 0; rstn = 1;
-    #5
-    clk = 1; rstn = 1;
-    #5
-    clk = 0; rstn = 1;
-    #5
-    clk = 1; rstn = 1;
-    #5
-    clk = 0; rstn = 1;
-    #5
-    clk = 1; rstn = 1;
+    #20
+     rstn = 1;
   end
 
 endmodule
