aa9ce2e87457f5bb1c9a942834331c721298ed45
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-20 13:00:28 +0500

    Add files via upload

---
+++ 16-bit Custom Processor/TestBench/tb_CL02.v
@@ -0,0 +1,22 @@
+module tb_CL02();
+//inputs
+  reg clk, rstn;
+//outputs
+  wire [7:0] PC_value;
+  wire [15:0] Instruction, Source_01, MUX_01_R, ALU_ans, MUX_02_R;
+
+//calling Control logic 02 module
+  CL_02 obj_CL02(clk, rstn, PC_value, Instruction, Source_01, MUX_01_R, ALU_ans, MUX_02_R);
+
+  always
+  #5
+  clk = !clk;
+
+  initial
+  begin
+    clk = 0; rstn = 0;
+    #20
+     rstn = 1;
+  end
+
+endmodule
