9ed0e0d137aeb699742dd7e0662c9685a609386f
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-20 16:33:39 +0500

    Update Special_Purpose_Register.v

--- 16-bit Custom Processor/Datapath/Special_Purpose_Register.v
+++ 16-bit Custom Processor/Datapath/Special_Purpose_Register.v
@@ -1,7 +1,22 @@
-module SPR(SR,Lo,Hi,clk);
+module SPR(clk, rstn, SR, Lo, Hi);
+  input clk, rstn;
+//special purpose registers
   output [8:0] SR;
   output [15:0] Lo;
   output [15:0] Hi;
-  input clk;
+
+  reg [8:0] SR;
+  reg [15:0] Lo;
+  reg [15:0] Hi;
  
+  always @(posedge clk or negedge rstn)
+  begin
+    if(!rstn)
+    begin
+//initialize the registers
+       SR <= 0;
+       Lo <= 0;
+       Hi <= 0;
+    end
+  end
 endmodule
