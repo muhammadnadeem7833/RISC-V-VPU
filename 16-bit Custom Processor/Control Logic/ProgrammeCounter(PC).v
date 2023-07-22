109170456f115c4890168ec9ab8854ff98a48ffa
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-22 15:51:23 +0500

    Update ProgrammeCounter(PC).v

--- 16-bit Custom Processor/Control Logic/ProgrammeCounter(PC).v
+++ 16-bit Custom Processor/Control Logic/ProgrammeCounter(PC).v
@@ -1,31 +1,24 @@
-module PC(PC_select, Jump_address, clk, rstn, current_addr);
+module PC(PC_select, Jump_address, clk, rstn, instr_addr);
 
   input PC_select; //flag to differ between simple & J-type instructions
   input rstn, clk;
   input [7:0] Jump_address; //address to be jumped on
-  output [7:0] current_addr; //current instruction's address //by-default wire
+  output [7:0] instr_addr; //current instruction's address //by-default wire
 
-  reg [7:0] next_addr; //next instruction's address
-  reg [7:0] temp;
+  reg [7:0] instr_addr;
 
 
   always@(posedge clk or negedge rstn)//rstn is used here as active low
   begin
     if (!rstn)//rstn == 0 reset the circuit
-    begin 
-      next_addr <= 0;
-      temp <= 0;
-    end
-    else 
+      instr_addr <= 0;
+    else  //rstn == 1
     begin
-      temp <= next_addr;
-      if(PC_select)      //rstn == 1 & when there is the Jump instruction
-        next_addr <= Jump_address;
+      if(PC_select)      //when there is the Jump instruction
+        instr_addr <= Jump_address;
       else
-        next_addr <= temp + 1;    //rstn == 1 & when non-jump instruction
+        instr_addr <= instr_addr + 1;    //when non-jump instruction
     end
   end
-
-  assign current_addr = temp;
 
 endmodule
