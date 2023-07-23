a6863f282897745d7b10f833555a01bb50557163
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-24 00:10:49 +0500

    Update tb_Intermediate_Control_Logic.v

--- 16-bit Custom Processor/TestBench/tb_Intermediate_Control_Logic.v
+++ 16-bit Custom Processor/TestBench/tb_Intermediate_Control_Logic.v
@@ -6,12 +6,14 @@ module tb_CL01();
   wire [15:0] Instruction;
   wire  PC_select;
   wire [7:0] Jump_addr;
-  wire Source2_select, ALU_out_Select, RegWrite_Flag;
+  wire [1:0] Source2_select;
+  wire ALU_out_Select, RegWrite_Flag, Rd_select, Mul_flag;
   wire [5:0] Immediate_Addr;
+  wire [2:0] shift_amm;
   wire [2:0] Rreg_Sig1, Rreg_Sig2, Wreg_Sig;
   wire [3:0] OPCODE;
 
-  Control_logic_01 obj_CL01(clk, rstn, instr_addr, Instruction, PC_select, Source2_select, ALU_out_Select, RegWrite_Flag, Jump_addr, Immediate_Addr, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
+  Control_logic_01 obj_CL01(clk, rstn, instr_addr, Instruction, PC_select, Source2_select, ALU_out_Select, RegWrite_Flag, Rd_select, Mul_flag, Jump_addr, Immediate_Addr, shift_amm, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
 
   always
   #5
