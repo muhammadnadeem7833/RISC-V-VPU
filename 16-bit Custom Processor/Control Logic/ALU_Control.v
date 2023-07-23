04ff220cb2cb5bc8f804e8c6c9ca8c051a392d49
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-24 00:04:49 +0500

    Update ALU_Control.v

--- 16-bit Custom Processor/Control Logic/ALU_Control.v
+++ 16-bit Custom Processor/Control Logic/ALU_Control.v
@@ -1,19 +1,23 @@
 module ALU_CON(opcode, signal);
   input [3:0] opcode; //4-bit opcode as input
-  output [1:0] signal; //2-bit operation selection signal for ALU
+  output [2:0] signal; //3-bit operation selection signal for ALU
 
-  reg [1:0] signal;
+  reg [2:0] signal;
 
   always @(*)
   begin
     if(opcode == 4'b0011) //if or instruction
-      signal = 2'b11;	
+      signal = 3'b011;	
     else if(opcode == 4'b0100) //if and instruction
-      signal = 2'b10;	
+      signal = 3'b010;	
     else if(opcode == 4'b1010) //if mul instruction
-      signal = 2'b01;
+      signal = 3'b001;
+    else if(opcode == 4'b0001) //if sll instruction
+      signal = 3'b100;
+    else if(opcode == 4'b0010) //if slr instruction
+      signal = 3'b101;
     else  //otherwise ALU performs addition
-      signal = 2'b00;	
+      signal = 3'b000;	
   end
 
 endmodule
