4b06b629291991ce02d6cb98cd13f9eb6ff9e775
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-24 00:11:13 +0500

    Update tb_Main_Control_Logic.v

--- 16-bit Custom Processor/TestBench/tb_Main_Control_Logic.v
+++ 16-bit Custom Processor/TestBench/tb_Main_Control_Logic.v
@@ -3,10 +3,11 @@ module tb_CL02();
   reg clk, rstn;
 //outputs
   wire [7:0] PC_value;
-  wire [15:0] Instruction, Source_01, MUX_01_R, ALU_ans, MUX_02_R;
+  wire [15:0] Instruction, Source_01, MUX_01_R, ALU_ans, MUX_02_R, MUX_03_R;
+  wire [2:0] ALU_Signal;
 
 //calling Control logic 02 module
-  CL_02 obj_CL02(clk, rstn, PC_value, Instruction, Source_01, MUX_01_R, ALU_ans, MUX_02_R);
+  CL_02 obj_CL02(clk, rstn, PC_value, Instruction, Source_01, MUX_01_R, ALU_ans, MUX_02_R, MUX_03_R, ALU_Signal);
 
   always
   #5
