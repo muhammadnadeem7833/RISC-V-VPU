38c0396162aa6bba9a12137ead40f3507f1dae2f
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-24 00:09:32 +0500

    Update Intermediate_Control_Logic.v

--- 16-bit Custom Processor/Integration/Intermediate_Control_Logic.v
+++ 16-bit Custom Processor/Integration/Intermediate_Control_Logic.v
@@ -1,11 +1,14 @@
-module Control_logic_01(clk, rstn, instr_addr, Instruction, PC_select, Source2_select, ALU_out_Select, RegWrite_Flag, Jump_addr, Immediate_Addr, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
+module Control_logic_01(clk, rstn, instr_addr, Instruction, PC_select, Source2_select, ALU_out_Select, RegWrite_Flag, Rd_select, Mul_flag, Jump_addr, Immediate_Addr, shift_amm, Rreg_Sig1, Rreg_Sig2, Wreg_Sig, OPCODE);
   input clk, rstn; //inputs
  //outputs of module
   output [7:0] instr_addr;
   output [15:0] Instruction;
-  output PC_select, Source2_select, ALU_out_Select, RegWrite_Flag;
+  output PC_select;
+  output  [1:0] Source2_select;
+  output ALU_out_Select, RegWrite_Flag, Rd_select, Mul_flag;
   output [7:0] Jump_addr;
   output [5:0] Immediate_Addr;
+  output [2:0] shift_amm;
   output [2:0] Rreg_Sig1, Rreg_Sig2, Wreg_Sig;
   output [3:0] OPCODE;
 
@@ -22,22 +25,29 @@ module Control_logic_01(clk, rstn, instr_addr, Instruction, PC_select, Source2_s
   wire [15:0] instruction1;
   IR obj_IR(instruction, clk, rstn, instruction1);
 
-  wire source2_select, ALU_out_select,regwrite_flag;
+  wire [1:0] source2_select;
+  wire ALU_out_select, regwrite_flag, Rd_Select, mul_flag;
   wire [5:0] immediate_addr;
+  wire [2:0] Shift_amm;
   wire [2:0] Rreg_sig1, Rreg_sig2, Wreg_sig;
   wire [3:0] opcode;
-  Decode obj_Dec(instruction, pc_select, source2_select, ALU_out_select, regwrite_flag, jump_addr, immediate_addr, Rreg_sig1, Rreg_sig2, Wreg_sig, opcode);
+  Decode obj_Dec(instruction1, pc_select, source2_select, ALU_out_select, regwrite_flag, Rd_Select, mul_flag, jump_addr, immediate_addr, Shift_amm, Rreg_sig1, Rreg_sig2, Wreg_sig, opcode);
+
 //assigning output 
   assign instr_addr = inst_address;
-  assign Instruction = instruction;
+  assign Instruction = instruction1;
   assign PC_select = pc_select;
   assign Source2_select = source2_select;
   assign ALU_out_Select = ALU_out_select;
   assign RegWrite_Flag = regwrite_flag;
+  assign Rd_select = Rd_Select;
+  assign Mul_flag = mul_flag;
   assign Jump_addr = jump_addr;
   assign Immediate_Addr = immediate_addr;
+  assign shift_amm = Shift_amm;
   assign Rreg_Sig1 = Rreg_sig1;
   assign Rreg_Sig2 = Rreg_sig2;
   assign Wreg_Sig = Wreg_sig;
   assign OPCODE = opcode;
+
 endmodule
