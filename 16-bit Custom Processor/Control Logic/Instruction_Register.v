5093681e5a32df82672ab0cb0148f5405d6d1cee
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-24 00:06:36 +0500

    Update Instruction_Register.v

--- 16-bit Custom Processor/Control Logic/Instruction_Register.v
+++ 16-bit Custom Processor/Control Logic/Instruction_Register.v
@@ -3,16 +3,14 @@ module IR(in_instr, clk, rstn, out_instr);
   input clk, rstn;
   
   output [15:0] out_instr; //output wire of 16-bit instruction
-  reg [15:0] instr_reg;
+  reg [15:0] out_instr;
 
   always @(posedge clk or negedge rstn) //run on positive edge of every clock cycle
   begin
     if(!rstn)        //if reset == 0, reset registr value
-      instr_reg <= 0;
+      out_instr <= 0;
     else
-      instr_reg <= in_instr; //making memory
+      out_instr <= in_instr; //making memory
   end
-
-  assign out_instr = instr_reg; //output instruction wire
 
 endmodule
