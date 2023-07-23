6621db05a8966e5f26cfb65c56a2e936aeb1ddfa
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-07-24 00:09:46 +0500

    Update MUX_01_S2_select.v

--- 16-bit Custom Processor/Integration/MUX_01_S2_select.v
+++ 16-bit Custom Processor/Integration/MUX_01_S2_select.v
@@ -1,6 +1,7 @@
-module MUX_1(Source2_select, Dout2, Ext_Immed_Addr, out);
-  input Source2_select; //select line
-  input [15:0] Dout2, Ext_Immed_Addr; //2 options for MUX
+module MUX_1(Source2_select, Dout2, Ext_Immed_Addr, shift_amm, out);
+  input [1:0] Source2_select; //select line
+  input [15:0] Dout2, Ext_Immed_Addr;
+  input [2:0] shift_amm; //3rd input of shift ammount in case of sll or slr
 
   output [15:0] out; //result of MUX
 
@@ -8,10 +9,11 @@ module MUX_1(Source2_select, Dout2, Ext_Immed_Addr, out);
 
   always @(*)
   begin
-    if(Source2_select)  //if select line == 1, immediate address will go
-      out = Ext_Immed_Addr;  
-    else
-      out = Dout2; //if select line == 0, source-2 Data will go
+    case(Source2_select)
+    2'b00: out = Dout2; //if select line == 0, source-2 Data will go
+    2'b01: out = Ext_Immed_Addr; //if select line == 1, immediate address will go 
+    2'b10: out = shift_amm; //if select line == 2, shift ammount will go
+    endcase     
   end
 
 endmodule
