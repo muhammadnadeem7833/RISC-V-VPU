d4d6389dff23bcbee28ce221c3c0e9d02de9b779
muhammadnadeem7833 <124807084+muhammadnadeem7833@users.noreply.github.com>
2023-10-07 15:40:23 +0500

    ALU.v

--- 16-bit Custom Processor/Datapath/ALU.v
+++ 16-bit Custom Processor/Datapath/ALU.v
@@ -1,1 +1,23 @@
+module ALU(in1, in2, con_sig, out);
+//2 operands of 16-bit
+  input [15:0] in1;
+  input [15:0] in2;
 
+  input [2:0] con_sig; //operation selection bits
+  output [31:0] out; //result
+
+  reg [31:0] out;
+
+  always @(*)
+  begin
+    case(con_sig)
+    2'b000: out = in1 + in2; //addition
+    2'b001: out = in1 * in2; //multiplication
+    2'b010: out = in1 & in2; //and
+    2'b011: out = in1 | in2; //or
+    2'b100: out = in1 << in2; //sll
+    2'b101: out = in1 >> in2; //slr
+    endcase
+  end
+
+endmodule
